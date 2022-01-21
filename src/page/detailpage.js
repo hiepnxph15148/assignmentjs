890bd0c37dc75e96a548f2450e6281596ce54b73
hiepnxph <hiepnxph@gmail.com>
2022-01-21 23:35:30 +0700

    uplate

--- src/page/detailpage.js
+++ src/page/detailpage.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
 import { get } from '../api/post';
-const DetailNewsPage = {
-     async render(id) {
-           const { data } = await get(id);
+import MenuList from '../components/menuList';
+import Footer from '../components/footer';
+
+const DetailPost = {
+    async render(id) {
+        const { data } = await get(id);
         return `
-        <div class="max-w-5xl m-auto">
-            ${MenuList.print()}
-            <h1 class="text-center py-2 font-bold">${result.title}</h1>
-            <img src="${result.img}" class="m-auto />
-            <p text-center py-4>${result.desc}</p>
-           <spam class="backdrop-grayscale-400"> ${Footer.print()} </spam>
+            <div>
+            <div class="max-w-5xl m-auto">
+            ${MenuList.render()}
+            <h1 class="text-center py-2 font-bold">${data.title}</h1>
+            <img src="${data.img}" class="m-auto />
+            <p text-center py-4>${data.desc}</p>
+           <spam class="backdrop-grayscale-400"> ${Footer.render()} </spam>
         </div>    
-        `;
+            </div>
+        `
     },
 };
-export default DetailNewsPage;
+export default DetailPost;
